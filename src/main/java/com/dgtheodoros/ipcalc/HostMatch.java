84d483f74f0bb7b3a1d6a02e9a1e097d306dcf87
Theo <Theo@VAIO>
2020-12-30 20:28:55 +0200

    commit 30/12/20 20:30

--- src/main/java/com/dgtheodoros/ipcalc/HostMatch.java
+++ src/main/java/com/dgtheodoros/ipcalc/HostMatch.java
@@ -2,6 +2,20 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *This file is part of IPCalc.
+ *
+ *    IPCalc is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    IPCalc is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with IPCalc.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.dgtheodoros.ipcalc;
 
